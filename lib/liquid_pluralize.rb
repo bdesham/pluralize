ae2dfec0c6fda0bc7d95966e0dd4b60b1eb14d01
Benjamin Esham <benjamin@esham.io>
2015-03-04 07:30:32 -0500

    Style tweaks

--- lib/liquid_pluralize.rb
+++ lib/liquid_pluralize.rb
@@ -1,10 +1,10 @@
 module Pluralize
 
-	def pluralize(number, singular, plural=nil)
+	def pluralize(number, singular, plural = nil)
 		number = number.to_i
 		if number == 1
 			"#{number} #{singular}"
-		elsif plural == nil
+		elsif plural.nil?
 			"#{number} #{singular}s"
 		else
 			"#{number} #{plural}"
